4074d3154224a9727105792a8f41d90f649fbf10
Jesse <jessehglvn@gmail.com>
2021-09-04 22:19:14 -0400

    added webpack server and hot inject

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,6 +3,10 @@ import '../styles/styles.css'
 import Banner from './modules/Banner'
 import Request from './modules/Request'
 
+if(module.hot) {
+    module.hot.accept()
+}
+
 new Banner()
 new Request()
 
--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,14 @@ module.exports = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'app'),
+        },
+        hot: true,
+        port: 3000
+    },
     mode: 'development',
-    watch: true,
     module: {
         rules: [
             {
